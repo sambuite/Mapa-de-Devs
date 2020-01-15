87b04c462ce8e2d760746eeb684b5b93e3df589c
sambuite <murilosambuite@gmail.com>
2020-01-15 20:10:36 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,6 +9,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-dxdox.mongodb.net/w
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
