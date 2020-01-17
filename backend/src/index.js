5d8e9ebec77226127eedf80f5fbfd0b3a0519712
sambuite <murilosambuite@gmail.com>
2020-01-17 16:54:39 -0300

    remove DB link

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-dxdox.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://...', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
