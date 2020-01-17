5d2bbabb903b2c40e697a5ea0f8014e005442665
sambuite <murilosambuite@gmail.com>
2020-01-17 16:44:45 -0300

    add socket.io

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import { MaterialIcons } from '@expo/vector-icons';
 
 import api from '../services/api';
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket';
 
 function Main({ navigation }){
   const [devs, setDevs] = useState([]);
@@ -33,6 +34,22 @@ function Main({ navigation }){
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]));
+  }, [devs]);
+
+  function setupWebsocket(){
+    disconnect();
+
+    const { latitude, longitude } = currentRegion;
+
+    connect(
+      latitude,
+      longitude,
+      techs
+    );
+  }
+
   async function loadDevs(){
     const { latitude, longitude } = currentRegion; 
 
@@ -45,6 +62,7 @@ function Main({ navigation }){
     });
 
     setDevs(response.data);
+    setupWebsocket();
   }
 
   function handleRegionChanged(region){
